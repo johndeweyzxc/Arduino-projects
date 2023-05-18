e7a0b0ea09ed02b2468182a810f4c5ea769d3412
johndeweyzxc <johndewey02003@gmail.com>
2023-05-18 22:04:18 +0800

    Configured static local ip address, gateway, primary dns and secondary dns

--- NodeMCU-3-Led/NodeMCU-3-Led.ino
+++ NodeMCU-3-Led/NodeMCU-3-Led.ino
@@ -27,6 +27,15 @@ unsigned long previousTime = 0;
 // Define timeout time in milliseconds (example: 2000ms = 2s)
 const long timeoutTime = 2000;
 
+// Set your Static IP address
+IPAddress local_IP(192, 168, 54, 26);
+// Set your Gateway IP address
+IPAddress gateway(192, 168, 1, 1);
+
+IPAddress subnet(255, 255, 0, 0);
+IPAddress primaryDNS(8, 8, 8, 8);   //optional
+IPAddress secondaryDNS(8, 8, 4, 4); //optional
+
 void setup() {
   Serial.begin(9600);
   NodeMCU.begin(4800);
@@ -38,6 +47,11 @@ void setup() {
   digitalWrite(D1, LOW);
   digitalWrite(D2, LOW);
   digitalWrite(D3, LOW);
+
+  // Configures static IP address
+  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+    Serial.println("STA Failed to configure");
+  }
 
   NodeMCU.println("Testing...");
   // Connect to Wi-Fi network with SSID and password
